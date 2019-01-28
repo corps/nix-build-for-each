3699512782e8dbc643657bec31bd7dc1577ef4cc
Zach Collins <recursive.cookie.jar@gmail.com>
2019-01-28 13:17:11 -0800

    Add gnugrep as explicit dependency

--- default.nix
+++ default.nix
@@ -1,16 +1,23 @@
 { pkgs ? import <nixpkgs> {}
 , bash ? pkgs.bash
 , stdenv ? pkgs.stdenv
+, gnugrep ? pkgs.gnugrep
+, makeWrapper ? pkgs.makeWrapper
 }:
 
 stdenv.mkDerivation {
   name = "nix-build-for-each";
   src = ./.;
-  buildInputs = [ bash ];
+  buildInputs = [ bash gnugrep makeWrapper ];
 
   installPhase = ''
     mkdir -p $out/bin/
     chmod +x nix-*
     cp nix-* $out/bin/
+
+	  for file in $out/bin/*; do
+      wrapProgram $file \
+        --prefix PATH ":" "${gnugrep}/bin"
+    done
   '';
 }
